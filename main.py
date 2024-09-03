6dcefa1a5774bd354a073dd462eed0b7c0ceb59c
Nikunj Sharma <57298335+nouveau-riche@users.noreply.github.com>
2024-09-03 22:03:59 +0530

    s3 integration

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from routers import event_router, news_router, offer_router, user_router
 
 app = FastAPI()
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(event_router.router)
 app.include_router(news_router.router)
 app.include_router(offer_router.router)
 app.include_router(user_router.router)
-
-
-# // work on adding user image while integrating s3
 
 
 @app.get('/')
--- routers/user_router.py
+++ routers/user_router.py
@@ -1,22 +1,29 @@
-from fastapi import APIRouter, Query
+from fastapi import APIRouter, Query, UploadFile, Form
 from typing import Annotated
 
+from utils.s3_uploads import uploadImage
 from schemas.user_schemas import User
 from serializers.user_serializer import userSerializer
 from database.database import user_collection as db
 
 router = APIRouter(prefix='/auth')
 
+
 @router.post('/create-user')
 async def createUser(payload: User):
     result = db.insert_one(payload.model_dump())
     new_user = userSerializer(db.find_one({'_id': result.inserted_id}))
-    return {'data': new_user}   
+    return {'data': new_user}
+
 
 @router.get('/get-user')
 async def getUser(firebase_uid: Annotated[str, Query()]):
-    result = userSerializer( db.find_one({'firebase_uid': firebase_uid}))
+    result = userSerializer(db.find_one({'firebase_uid': firebase_uid}))
     return {'data': result}
 
 
-
+@router.post('/upload-user-profile-image')
+async def uploadUserProfileImage(file: UploadFile, firebase_uid: Annotated[str, Form()]):
+    image = await uploadImage(file=file)
+    db.update_one({'firebase_uid': firebase_uid}, {"$set" : {'image': image}})
+    return {'data': 'Image uploaded successfully'}
